db23e3ca0e0c196fb610637162c3347f8d74d92f
GoobBot <uristmchands@proton.me>
2025-05-21 09:47:51 +0200

    limit increase

--- lokilogger/lokilogger.py
+++ lokilogger/lokilogger.py
@@ -132,7 +132,7 @@ class LokiLogger(commands.Cog):
             "query": query,
             "start": str(start_ts_ns + 1),
             "end": str(end_ts_ns),
-            "limit": 50, 
+            "limit": 500, 
             "direction": "forward",
         }
 
